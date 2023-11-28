bc8658a88c4b4fff1a0b76f45cfbd9ebc4096675
rugii913 <rugii913@gmail.com>
2023-11-28 15:19:10 +0900

    header.html, footer.html 분리, 필요할 때 html 불러올 수 있도록 함
    *header, footer 변경 시, 모든 html 파일 변경하지 않도록, 변경점을 집중 시킴

---
+++ static/js/includeHTML.js
@@ -0,0 +1,37 @@
+// <!-- https://kay0426.tistory.com/27 HTML 파일 분리하는 법 -->
+
+function includeHTML(callback) {
+    var z, i, element, file, xhr;
+    /*loop through a collection of all HTML elements:*/
+    z = document.getElementsByTagName("*");
+    for (i = 0; i < z.length; i++) {
+        element = z[i];
+        /*search for elements with a certain atrribute:*/
+        file = element.getAttribute("include-html");
+        //console.log(file);
+        if (file) {
+            /*make an HTTP request using the attribute value as the file name:*/
+            xhr = new XMLHttpRequest();
+            xhr.onreadystatechange = function () {
+                if (this.readyState == 4) {
+                    if (this.status == 200) {
+                        element.innerHTML = this.responseText;
+                    }
+                    if (this.status == 404) {
+                        element.innerHTML = "Page not found.";
+                    }
+                    /*remove the attribute, and call this function once more:*/
+                    element.removeAttribute("include-html");
+                    includeHTML(callback);
+                }
+            };
+            xhr.open("GET", file, true);
+            xhr.send();
+            /*exit the function:*/
+            return;
+        }
+    }
+    setTimeout(function () {
+        callback();
+    }, 0);
+}
